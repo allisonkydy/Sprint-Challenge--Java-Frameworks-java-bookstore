eb971b75b15aa1996062ba1fd107f3949594932c
allisonkydy <allisnelly@gmail.com>
2019-10-18 12:08:22 -0500

    made authors endpoints pageable

--- bookstore/src/main/java/com/lambdaschool/bookstore/controllers/AuthorController.java
+++ bookstore/src/main/java/com/lambdaschool/bookstore/controllers/AuthorController.java
@@ -2,6 +2,8 @@ package com.lambdaschool.bookstore.controllers;
 
 import com.lambdaschool.bookstore.services.AuthorService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,8 +18,8 @@ public class AuthorController
   // GET /authors/authors - returns a JSON object list of all the authors, their books, and the book's section.
   @GetMapping(value = "/authors/authors",
               produces = {"application/json"})
-  public ResponseEntity<?> listAllAuthors()
+  public ResponseEntity<?> listAllAuthors(@PageableDefault(page = 0, size = 3) Pageable pageable)
   {
-    return new ResponseEntity<>(authorService.findAll(), HttpStatus.OK);
+    return new ResponseEntity<>(authorService.findAll(pageable), HttpStatus.OK);
   }
 }
